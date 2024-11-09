7a23bdc4370bfd234c1f66704c16e63bcff86d34
Rowan Torbitzky-Lane <rat23h@umsystem.edu>
2024-11-09 11:35:28 -0600

    update stock regression

--- examples/stocks/stock_regression.py
+++ examples/stocks/stock_regression.py
@@ -17,6 +17,7 @@ the ReLU and LeakyReLU functions.
 """
 import random
 import pandas as pd
+import numpy as np
 
 from pyshgp.gp.estimators import PushEstimator
 from pyshgp.gp.genome import GeneSpawner
@@ -24,11 +25,28 @@ from pyshgp.gp.selection import Lexicase
 from pyshgp.push.instruction_set import InstructionSet
 from pyshgp.push.interpreter import PushInterpreter, MemoryInterpreter
 
-intc_data: pd.DataFrame = pd.read_csv("/home/user/Documents/CS6401/pyshgp/examples/stocks/intc_data.csv")
-inputs = intc_data["Close"].to_numpy()[:-1]
-outputs = intc_data["Close"].to_numpy()[1:]
-y = outputs.reshape([-1, 1])
-X = inputs.reshape([-1, 1])
+sliding_window_size: int = 50
+
+btc_usd_data: pd.DataFrame = pd.read_csv("/home/user/Documents/CS6401/pyshgp/examples/stocks/btc_usd_data.csv")
+btc_usd_data.drop(["Dividends", "Stock Splits", "Date"], inplace=True, axis=1)
+X: list[list[int]] = []
+y: list[int] = []
+for num in range(0, len(btc_usd_data) - sliding_window_size - 1):
+    X.append([])
+    for row in btc_usd_data[num:(num+sliding_window_size)].itertuples():
+        X[num].append(row.Open)
+        X[num].append(row.Close)
+        X[num].append(row.High)
+        X[num].append(row.Low)
+        X[num].append(row.Volume)
+        y.append(btc_usd_data["Close"][num+sliding_window_size+1])
+
+y = np.array(y).reshape(-1, 1)
+
+#inputs = btc_usd_data["Close"].to_numpy()[:-1]
+#outputs = btc_usd_data["Close"].to_numpy()[1:]
+#y = outputs.reshape([-1, 1])
+#X = inputs.reshape([-1, 1])
 # y = intc_data["Open"].to_numpy().reshape([-1, 1])
 # X = np.arange(0, len(y)).reshape([-1, 1])
 
@@ -70,7 +88,7 @@ spawner = GeneSpawner(
 
 
 ep_lex_sel = Lexicase(epsilon=True)
-interpreter: MemoryInterpreter = MemoryInterpreter(memory_size=10)# , dementia_amt=20)
+interpreter: MemoryInterpreter = MemoryInterpreter(memory_size=10 , dementia_amt=60)
 
 
 if __name__ == "__main__":
