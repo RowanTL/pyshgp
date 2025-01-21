6d35a523b4bb68d661f4f0a15d933e05f5cdcba5
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-01-21 17:02:29 -0600

    move tests.support code to interpreter

--- pyshgp/push/interpreter.py
+++ pyshgp/push/interpreter.py
@@ -4,7 +4,9 @@ from typing import Union
 import time
 from enum import Enum
 from copy import deepcopy
+import json
 
+from pyshgp.gp.genome import Genome
 from pyshgp.push.instruction import Instruction
 from pyshgp.push.program import Program
 from pyshgp.push.state import PushState
@@ -13,6 +15,7 @@ from pyshgp.push.atoms import Atom, Closer, Literal, InstructionMeta, CodeBlock,
 from pyshgp.push.config import PushConfig
 from pyshgp.tap import tap
 from pyshgp.validation import PushError
+from pyshgp.push.program import ProgramSignature
 
 
 class PushInterpreterStatus(Enum):
@@ -298,3 +301,45 @@ class InspectInterpreter(PushInterpreter):
             print("Finished program evaluation.")
 
         return states
+
+def _deserialize_atoms(lst, instr_set: InstructionSet):
+    type_lib = instr_set.type_library
+    atoms = []
+    for atom_spec in lst:
+        atom = None
+        if isinstance(atom_spec, list):
+            atom = CodeBlock(_deserialize_atoms(atom_spec, instr_set))
+        else:
+            atom_type = atom_spec["a"]
+            if atom_type == "close":
+                atom = Closer()
+            elif atom_type == "lit":
+                push_type = type_lib[atom_spec["t"]]
+                value = push_type.coerce(atom_spec["v"])
+                atom = Literal(value=value, push_type=push_type)
+            elif atom_type == "input":
+                atom = Input(input_index=atom_spec["i"])
+            elif atom_type == "instr":
+                instr = instr_set[atom_spec["n"]]
+                atom = InstructionMeta(name=instr.name, code_blocks=instr.code_blocks)
+            else:
+                raise ValueError("bad atom spec {s}".format(s=atom_spec))
+        atoms.append(atom)
+    return atoms
+
+
+def load_code(name, interpreter) -> CodeBlock:
+    with open("tests/resources/programs/" + name + ".json") as f:
+        atoms = _deserialize_atoms(json.load(f), interpreter.instruction_set)
+        return CodeBlock(atoms)
+
+
+def get_program(name: str, sig: ProgramSignature, interpreter: PushInterpreter):
+    cb = load_code(name, interpreter)
+    return Program(code=cb, signature=sig)
+
+
+def load_genome(name, interpreter) -> Genome:
+    with open("tests/resources/genomes/" + name + ".json") as f:
+        atoms = _deserialize_atoms(json.load(f), interpreter.instruction_set)
+        return Genome(atoms)
