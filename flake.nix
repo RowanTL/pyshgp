7283c6e74b81824b3b5baf4c9a90b533f555824b
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-04-24 20:11:53 -0500

    thx claude

--- flake.nix
+++ flake.nix
@@ -1,11 +1,9 @@
 {
   # May not be the most "nix" way to accomplish this, but works for now.
   description = "Basic PyshGP flake";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-
   outputs = { nixpkgs, ... }:
   let system = "x86_64-linux"; pkgs = import nixpkgs { system = "${system}"; config.allowUnfree = true; }; in
   {
@@ -22,9 +20,17 @@
             ];
           })
           jetbrains.pycharm-community
+          
+          # Add gcc and stdenv for libstdc++
+          gcc
+          stdenv.cc.cc.lib
         ];
         shellHook = ''
           export SHELL=${pkgs.lib.getExe pkgs.bashInteractive}
+          
+          # Add libstdc++ to LD_LIBRARY_PATH
+          export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+          
           python -m venv .venv
           source .venv/bin/activate
           pip install -r requirements-with-dev.txt
